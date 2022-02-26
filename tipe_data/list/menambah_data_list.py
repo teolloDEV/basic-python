3bc30e8437c18b6bb9e578d5ac8497dc510e25b8
nurarif151 <nurarif0151@gmail.com>
2022-02-27 03:12:32 +0700

    menambah data dengan append() dan insert(index, nilai)

---
+++ tipe_data/list/menambah_data_list.py
@@ -0,0 +1,16 @@
+# Menambah data di belakang
+# append ()
+
+list_buah = ['Jeruk', 'Naga', 'Pepaya', 'Mangga']
+print(list_buah)
+list_buah.append('semangka')
+print('\nsetelah data di tambah\n')
+print(list_buah)
+
+# Menambah data di depan
+# insert (index, nilai)
+
+print('menambah data di depan dengan insert ()')
+list_buah.insert(0, 'anggur')
+print('setelah ditambah')
+print(list_buah)
