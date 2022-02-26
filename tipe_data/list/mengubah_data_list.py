7757e344615e5dec595552c81874fcfa937493cd
nurarif151 <nurarif0151@gmail.com>
2022-02-27 03:01:39 +0700

    mengubah data pada list

---
+++ tipe_data/list/mengubah_data_list.py
@@ -0,0 +1,7 @@
+list_buah = ['Pisang', 'Nanas', 'Melon', 'Durian']
+# mengubah data pertama
+list_buah[0] = 'mengkudu'
+print(list_buah)
+# mengubah data terakhir
+list_buah[-1] = 'apel'
+print(list_buah)
