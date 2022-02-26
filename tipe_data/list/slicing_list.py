440b7c88849240065fe2a70d52ae453d153a1e0b
nurarif151 <nurarif0151@gmail.com>
2022-02-27 02:34:03 +0700

    slicing list : memotong nilai pada list

---
+++ tipe_data/list/slicing_list.py
@@ -0,0 +1,8 @@
+# memotong nilai pada list
+
+list_buah = ['Pisang', 'Nanas', 'Melon', 'Durian']
+
+# parameter kiri awal dari index yang akan ditampilkan
+# parameter kanan berarti batas yang akan di tapilkan
+
+print(list_buah[0:3])
