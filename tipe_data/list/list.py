dafcc8136416738006742ab26f0a5e98d6b9d617
nurarif151 <nurarif0151@gmail.com>
2022-02-27 02:19:59 +0700

    menampilkan data list dengan index

---
+++ tipe_data/list/list.py
@@ -0,0 +1,7 @@
+# list di definisikan dengan kurung siku [ ]
+
+list_buah = ['apel', 'mangga', 'jambu', 'jeru', 'sirsak']
+print(list_buah)
+
+
+
---
+++ tipe_data/list/menampilkan_data_index.py
@@ -0,0 +1,11 @@
+# menampilkan isi list tertentu dengan menggunakan index
+
+list_buah = ['apel', 'jeruk', 'mangga', 'alpukat']
+print(list_buah[0]) #apel
+print(list_buah[3]) #alpukat
+print(list_buah)
+
+# menampilkan data dari belakang dengan index negatif
+
+print(list_buah[-1]) # alpukat
+print(list_buah[-2]) # mangga
