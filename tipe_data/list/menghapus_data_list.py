f54a0835b4601277786875b345c4929265470b4a
nurarif151 <nurarif0151@gmail.com>
2022-02-27 03:22:37 +0700

    menggunakan del untuk menghapus pada index berapapun pada list

--- tipe_data/list/menghapus_data_list.py
+++ tipe_data/list/menghapus_data_list.py
@@ -2,6 +2,7 @@
 # menghapus dan mengambil angka terkhir
 # data bisa disimpan di pop()
 
+print('menghapus dengan fungsi pop() ')
 list_angka = [1, 2, 3, 4, 5]
 print(list_angka)
 hapus = list_angka.pop()
@@ -15,4 +16,10 @@ print('\nMenghapus dengan fungsi remove')
 list_buah = ['Mangga', 'Jambu', 'Jeruk', 'Jambu']
 print(list_buah)
 list_buah.remove('Jeruk')
+print(list_buah)
+
+print('\nMenghapus dengan fungsi del')
+list_buah = ['Mangga', 'Jambu', 'Jeruk', 'Jambu']
+print(list_buah)
+del list_buah[:1]
 print(list_buah)
