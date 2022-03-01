62e5dafd8b9d16dab4d746495290af8a86918e7d
nurarif151 <nurarif0151@gmail.com>
2022-03-01 14:18:53 +0700

    implementasi algoritma selection sort

---
+++ algoritma/selection_sort/selection_sort.py
@@ -0,0 +1,14 @@
+def selection_sort(data):
+    for i in range(len(data) - 1):
+        index = i
+        for j in range(i+1, len(data)):
+            if data[i] > data[j]:
+                index = j
+        temp = data[i]
+        data[i] = data[index]
+        data[index] = temp
+    print('iterasi ', i, data)
+
+
+data = [4, 3, 5, 6, 2, 78, 98]
+selection_sort(data)
--- main.py
+++
@@ -1,3 +0,0 @@
-angka = [6, 1, 8, 9, 44, 33, 11]
-for i in range(len(angka)-1):
-    print(angka)
