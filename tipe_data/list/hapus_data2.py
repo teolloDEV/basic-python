ca4eb576ba8701aad81c17494a876331d1b0d11b
nurarif151 <nurarif0151@gmail.com>
2022-03-10 15:17:29 +0700

    remove() | mengambil data yang mempunyai nilai sama dengan parameter

---
+++ tipe_data/list/hapus_data2.py
@@ -0,0 +1,9 @@
+# remove() | Menghapus data yang mempunyai nilai sama dengan parameter
+
+
+list_buah = ['Mangga', 'Jambu', 'Jeruk', 'Jambu']
+print(list_buah)
+
+
+list_buah.remove('Mangga')
+print(list_buah)
