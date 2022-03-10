1b3f8d352cfad90ec4233ed5ed2db480da14b16c
nurarif151 <nurarif0151@gmail.com>
2022-03-10 15:11:55 +0700

    pop() | mengambil angka terakhir pada list

---
+++ tipe_data/list/hapus_data1.py
@@ -0,0 +1,11 @@
+# pop() | Mengambil item terakhir dari list
+# fungsinya mengambil maka hasil kembalianya bisa disimpan divariabel
+
+
+list_angka = [1, 2, 3, 4, 5]
+print(list_angka)
+
+# Angka yang di ambil
+pop = list_angka.pop()
+print('angka yang terhapus : ', pop)
+print(list_angka)
