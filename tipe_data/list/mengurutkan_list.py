485a496de5e84906d89373ac5b11829ce4e208ea
nurarif151 <nurarif0151@gmail.com>
2022-03-10 15:25:44 +0700

    mengurutkan dan membalikkan list | sort() dan reverse()

---
+++ tipe_data/list/mengurutkan_list.py
@@ -0,0 +1,12 @@
+list_buah = ['Mangga', 'Jeruk', 'Zaitun', 'Apel', 'Durian']
+print(list_buah)
+
+# Untuk mengurutkan bisa dengan memanggil fungsi sort
+
+list_buah.sort()
+print(list_buah)
+
+# Membalikan list
+
+list_buah.reverse()
+print(list_buah)
