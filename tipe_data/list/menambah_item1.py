10a2ad806fad1319a8fb10c70328c3ce3eaf1c7a
nurarif151 <nurarif0151@gmail.com>
2022-03-10 15:05:40 +0700

    isert() | Menerima dua parameter (index, nilai)

---
+++ tipe_data/list/menambah_item1.py
@@ -0,0 +1,10 @@
+list_buah = ['Jeruk', 'Naga', 'Pepaya', 'Mangga']
+print(list_buah)
+
+# Menerima 2 parameter
+# prameter pertama mendefinisikan posisi index
+# parameter kedua mendefinisikan nilai yang akan dimasukkan kedalam list
+
+list_buah.insert(2, 'alpukat')
+print('setelahdi tambah dengan "insert()"')
+print(list_buah)
