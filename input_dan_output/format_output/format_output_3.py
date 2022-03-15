5b787d121d0d6d21072f57420fb029126bf72585
nurarif151 <nurarif0151@gmail.com>
2022-03-15 11:25:45 +0700

    format output | format dengan kunci

--- input_dan_output/format_output/format_output_3.py
+++ input_dan_output/format_output/format_output_3.py
@@ -1,5 +1,7 @@
-print('Halo {namaDepan} {namaBelakang}'.format(namaDepan='Agus', namaBelakang='Priyono'))
+print('Halo {namaDepan} {namaBelakang}'
+      .format(namaDepan='Agus', namaBelakang='Priyono'))
 # Halo Agus Priyono
 
-print('Halo {namaDepan} {namaBelakang}'.format(namaBelakang='Priyono', namaDepan='Agus'))
+print('Halo {namaDepan} {namaBelakang}'
+      .format(namaBelakang='Priyono', namaDepan='Agus'))
 # Halo Agus Priyono
