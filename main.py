06711b6929266201374ea15e4a1b7d2fcd8e1e4a
nurarif151 <nurarif0151@gmail.com>
2022-03-20 15:08:33 +0700

    percabangan

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-# membuat list
+panjang = int(input('masukkan panjang : '))
+lebar = int(input('masukkan lebar : '))
 
-list_kosong = []
-list_string = ['jakarta', 'bandung', 'jepara', 'bali']
-list_integer = [1, 2, 3, 4, 5, 6, 7, 8]
-
-print(list_kosong)
-print(list_string)
-print(list_integer)
+luas = panjang * lebar
+print(luas)
