f98f8aa7d3aced7b2b97734f4113cefcd221c672
nurarif151 <nurarif0151@gmail.com>
2022-03-21 10:09:31 +0700

    Mencari nilai tengah dari 3 angka

--- percabangan/Latihan/latihan2.py
+++ percabangan/Latihan/latihan2.py
@@ -5,7 +5,6 @@ a, b, c = (
     int(input('Masukkan nilai B : ')),
     int(input('Masukkan nilai C : '))
 )
-
 if (b > a > c) or (c > a > b) :
     print('A menjadi nilai tengah')
 elif ( a > b > c) or (c > b > a):
