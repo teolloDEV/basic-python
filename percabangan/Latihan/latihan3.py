8285284e1a68ba58a69d55af11ddc01ba65c56f9
nurarif151 <nurarif0151@gmail.com>
2022-03-21 10:15:14 +0700

    cek bilangan ganjil genap

---
+++ percabangan/Latihan/latihan3.py
@@ -0,0 +1,5 @@
+angka = int(input('Masukkan angka : '))
+if angka % 2 == 0 :
+    print('Angka yang anda masukkan adalah bilangan genap')
+else:
+    print('Angka yang anda masukkan adalah bilangan ganjil')
