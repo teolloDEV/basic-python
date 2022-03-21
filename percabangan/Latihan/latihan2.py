41f422933b6b0b73ab16f55682513abfa66abbdc
nurarif151 <nurarif0151@gmail.com>
2022-03-21 10:08:50 +0700

    Mencari nilai terbesar dari 3 angka

---
+++ percabangan/Latihan/latihan2.py
@@ -0,0 +1,14 @@
+# Mencari nilai tengah dari 3 angka
+
+a, b, c = (
+    int(input('Masukkan nilai A : ')),
+    int(input('Masukkan nilai B : ')),
+    int(input('Masukkan nilai C : '))
+)
+
+if (b > a > c) or (c > a > b) :
+    print('A menjadi nilai tengah')
+elif ( a > b > c) or (c > b > a):
+    print('B menjadi nilai tengah')
+else:
+    print('C menjadi nilai tengah')
