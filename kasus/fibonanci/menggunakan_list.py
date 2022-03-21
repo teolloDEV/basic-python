94fb1789d34131157fab606dc78af98b8814896a
nurarif151 <nurarif0151@gmail.com>
2022-03-21 16:14:45 +0700

    fibonanci | menggunakan list

--- kasus/fibonanci/menggunakan_list.py
+++ kasus/fibonanci/menggunakan_list.py
@@ -1,11 +1,10 @@
 panjang = int(input('Masukkan panjang deret: '))
 
-fibo = [0, 1]
+fibonanci = [0,1]
+
 for i in range(2, panjang):
-    angka1 = fibo[i - 2]
-    angka2 = fibo[i - 1]
-    angkaSelanjutnya = angka1 + angka2
-
-    fibo.append(angkaSelanjutnya)
-
-print(fibo)
+    angka1 = fibonanci[i - 2]
+    angka2 = fibonanci[i - 1]
+    angka_selanjutnya = angka1 + angka2
+    fibonanci.append(angka_selanjutnya)
+print(fibonanci)
