84d1952d6d920c426945db6cab2d14d7956003d5
nurarif151 <nurarif0151@gmail.com>
2022-03-22 10:14:55 +0700

    Tukar 2 variabel dengan variabel bantuan

---
+++ kasus/Menukar Dua Variabel/tukar_2variabel.py
@@ -0,0 +1,12 @@
+gelasA = 'kopi'
+gelasB = 'susu'
+
+print('[Nilai Awal]')
+print(f'gelasA = {gelasA}, gelasB = {gelasB}\n')
+
+gelasc = gelasA
+gelasA = gelasB
+gelasB = gelasc
+
+print('[Nilai Akhir]')
+print(f'gelasA = {gelasA}, gelasB = {gelasB}\n')
